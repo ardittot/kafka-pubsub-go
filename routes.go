280c634969d47c5168fcb100dad415ffa1180e55
Arditto Trianggada <atrianggada@gmail.com>
2018-01-06 21:55:49 +0700

    Update routes.go

--- routes.go
+++ routes.go
@@ -3,9 +3,9 @@ package main
 func initializeRoutes() {
 
   // Handle the index route
-  router.GET("/topic", GetTopics)
-  router.POST("/subscribe/add", AddConsumerTopic)
-  router.POST("/subscribe/delete", DeleteConsumerTopic)
+  router.GET("/subscribe/topic", GetTopics)
+  router.POST("/subscribe/topic/add", AddConsumerTopic)
+  router.POST("/subscribe/topic/delete", DeleteConsumerTopic)
   router.POST("/subscribe/url/add", AddConsumerUrl)
   router.POST("/subscribe/url/delete", DeleteConsumerUrl)
   router.POST("/publish/:topic", RunProduce)
